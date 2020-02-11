d32073c05cd0262cd15d46e57a3f8a1a50f2f0b6
Victor Vilela <vicor.vilela@outlook.com>
2020-02-11 13:38:03 -0500

    implements SqlServerPlugin.Log add a clientApp for MyLogger

---
+++ MyLogger/MyLogger.Core/Plugins/DbPlugins/DbParams.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLogger.Plugins.DbPlugins
+{
+    public class DbParams
+    {
+        public string ServerName { get; set; }
+        public string Database { get; set; }
+        public string UserId { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ MyLogger/MyLogger.Core/Shared/ConfigManager/IConfigManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLogger.Shared.ConfigManager
+{
+    public interface IConfigManager
+    {
+        string ServerName { get; }
+        string Database { get; }
+        string UserId { get; }
+        string Password { get; }
+    }
+}
