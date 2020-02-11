6ac22c0581203c6d1f799ad2ce4ec88434ddf7c4
Victor Vilela <vicor.vilela@outlook.com>
2020-02-11 13:56:50 -0500

    extract PatameterValidator class comment useless unit tests in SqlServerPluginUnitTests

---
+++ MyLogger/MyLogger.Core/Shared/ParameterValidator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLogger.Shared
+{
+    public class ParameterValidator
+    {
+        public static void ThowExceptionWhenIsNullOrEmpty(string paramValue, string paramName, string methodName)
+        {
+            if (string.IsNullOrEmpty(paramValue)) throw new ArgumentNullException(paramName, $"{methodName} : {paramName} is null or empty.");
+        }
+
+        public static void ThowExceptionWhenIsNull(object paramValue, string paramName, string methodName)
+        {
+            if (paramValue == null) throw new ArgumentNullException(paramName, $"{methodName} : {paramName} is null.");
+        }
+    }
+}
